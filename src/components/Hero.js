b6cee2a49014556c9e12e12ab7188fa40bb2d68a
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-08-08 12:43:18 +0300

    added

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -10,13 +10,23 @@ const Hero = () => {
     <section className='min-h-[618px] lg:min-h-[815px] bg-red-200 pt-[38px]'>
       <div className='container mx-auto'>
         <Nav />
-        <div className='w-[504px] h-[744px]'>
-          <Parallax
-            className='w-full h-full'
-            bgImage={Image}
-            bgImageAlt='guy'
-            strength={200}
-          ></Parallax>
+        <div className='flex items-center pt-32'>
+          <div className='flex-1'>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorem
+            autem assumenda inventore omnis iste natus ullam? Quo molestiae
+            earum nihil rem provident unde obcaecati incidunt itaque aut! Quod,
+            explicabo reiciendis!
+          </div>
+          <div className='flex-1'>
+            <div className='w-[504px] h-[744px]'>
+              <Parallax
+                className='w-full h-full p-8'
+                bgImage={Image}
+                bgImageAlt='guy'
+                strength={200}
+              ></Parallax>
+            </div>
+          </div>
         </div>
       </div>
     </section>
