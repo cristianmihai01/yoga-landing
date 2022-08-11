1849c5001c2ff5436f267dc6442deaba65013e53
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-08-11 10:34:27 +0300

    added

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,17 +3,20 @@ import React from 'react';
 // image 1
 import CardImage1 from '../assets/img/cards/card-1.png';
 import CardImage2 from '../assets/img/cards/card-2.png';
+import CardImage3 from '../assets/img/cards/card-3.png';
 
 const Cards = () => {
   return (
     <section className='bg-cardsBg min-h-[260px] pb-[55px] lg:-mt-24'>
       <div className='container mx-auto flex flex-col lg:flex-row gap-x-[32px]'>
-        {/* cards */}
+        {/* card group */}
         <div className='flex-1 flex gap-x-[15px] lg:gap-x-[32px] -mt-[38px] lg:-mt-[77px] z-10'>
           {/* card 1 */}
-          <div className='bg-white w-full max-w-[282px] p-[14px] lg:p-[26px] shadow-2xl rounded-md'>
+          <div className='bg-white w-full max-w-[282px] p-[14px] lg:p-[26px] shadow-2xl rounded-md max-h-[282px]'>
             <div className='flex items-center mb-[18px] lg:mb-[28px]'>
-              <h4 className='h4 mr-8'>Make Your Own Plan For Yoga.</h4>
+              <h4 className='text-sm leading-4 lg:text-2xl lg:leading-7 font-bold text-heading mr-8'>
+                Make Your Own Plan For Yoga.
+              </h4>
               <h2 className='h2 text-stroke-2'>1</h2>
             </div>
             <div>
@@ -23,7 +26,9 @@ const Cards = () => {
           {/* card 2 */}
           <div className='bg-white w-full max-w-[282px] p-[14px] lg:p-[26px] shadow-2xl rounded-md'>
             <div className='flex items-center mb-[18px] lg:mb-[28px]'>
-              <h4 className='h4 mr-8'>Find a Yoga Mentor For You.</h4>
+              <h4 className='text-sm leading-4 lg:text-2xl lg:leading-7 font-bold text-heading mr-8'>
+                Find a Yoga Mentor For You.
+              </h4>
               <h2 className='h2 text-stroke-2'>2</h2>
             </div>
             <div>
@@ -31,8 +36,24 @@ const Cards = () => {
             </div>
           </div>
         </div>
-        <div className='bg-white w-full max-w-[542px] p-[14px] lg:p-[26px] -mt-[38px] lg:-mt-[77px] z-10 shadow-2xl rounded-md'>
-          2
+        {/* card 3 */}
+        <div className='bg-white w-full max-w-[542px] p-[14px] lg:p-[26px] -mt-[38px] lg:-mt-[77px] z-10 shadow-2xl rounded-md flex justify-between items-start'>
+          <div className='max-w-[240px]'>
+            <div className='flex items-center mb-4 lg:mb-8'>
+              <h4 className='text-sm leading-4 lg:text-2xl lg:leading-7 font-bold text-heading mr-8'>
+                Every-Day Open Master Classes.
+              </h4>
+              <h2 className='h2 text-stroke-2'>3</h2>
+            </div>
+            <p>
+              We’re boosting online yoga by enabling anyone in the world to
+              learn from the best
+            </p>
+            <a href='#'>Learn more</a>
+          </div>
+          <div>
+            <img src={CardImage3} alt='' />
+          </div>
         </div>
       </div>
     </section>
