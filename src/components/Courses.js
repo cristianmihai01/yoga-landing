9207b282ac21b015e73f5751cde4862fa69c50ac
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-08-12 12:23:21 +0300

    added

--- src/components/Courses.js
+++ src/components/Courses.js
@@ -14,8 +14,18 @@ const Courses = () => {
       <div className='container mx-auto'>
         {/* text */}
         <div className='text-center mb-16 lg:mb-32'>
-          <h2 className='h2 mb-3 lg:mb-[18px]'>Popular Courses</h2>
-          <p className='max-w-[480px] mx-auto'>
+          <h2
+            className='h2 mb-3 lg:mb-[18px]'
+            data-aos='fade-down'
+            data-aos-delay='200'
+          >
+            Popular Courses
+          </h2>
+          <p
+            className='max-w-[480px] mx-auto'
+            data-aos='fade-down'
+            data-aos-delay='300'
+          >
             Practice anywhere, anytime. Explore a new way to exercise and learn
             more about yourself. We are providing the best.
           </p>
@@ -23,7 +33,11 @@ const Courses = () => {
         {/* course list */}
         <div className='flex flex-col lg:flex-row lg:gap-x-[33px] gap-y-24'>
           {/* item */}
-          <div className='w-full max-w-[368px] px-[18px] pb-[26px] lg:px-[28px] lg:pb-[38px] bg-white hover:shadow-primary flex flex-col rounded-[14px] mx-auto transition'>
+          <div
+            className='w-full max-w-[368px] px-[18px] pb-[26px] lg:px-[28px] lg:pb-[38px] bg-white hover:shadow-primary flex flex-col rounded-[14px] mx-auto transition'
+            data-aos='fade-up'
+            data-aos-delay='600'
+          >
             <div className='-mt-[38px] lg:-mt-12 mb-4 lg:mb-6'>
               <img src={CourseImg1} alt='' />
             </div>
@@ -51,7 +65,11 @@ const Courses = () => {
             </div>
           </div>
           {/* item */}
-          <div className='w-full max-w-[368px] px-[18px] pb-[26px] lg:px-[28px] lg:pb-[38px] bg-white hover:shadow-primary flex flex-col rounded-[14px] mx-auto transition'>
+          <div
+            className='w-full max-w-[368px] px-[18px] pb-[26px] lg:px-[28px] lg:pb-[38px] bg-white hover:shadow-primary flex flex-col rounded-[14px] mx-auto transition'
+            data-aos='fade-up'
+            data-aos-delay='700'
+          >
             <div className='-mt-[38px] lg:-mt-12 mb-4 lg:mb-6'>
               <img src={CourseImg2} alt='' />
             </div>
@@ -79,7 +97,11 @@ const Courses = () => {
             </div>
           </div>
           {/* item */}
-          <div className='w-full max-w-[368px] px-[18px] pb-[26px] lg:px-[28px] lg:pb-[38px] bg-white hover:shadow-primary flex flex-col rounded-[14px] mx-auto transition'>
+          <div
+            className='w-full max-w-[368px] px-[18px] pb-[26px] lg:px-[28px] lg:pb-[38px] bg-white hover:shadow-primary flex flex-col rounded-[14px] mx-auto transition'
+            data-aos='fade-up'
+            data-aos-delay='800'
+          >
             <div className='-mt-[38px] lg:-mt-12 mb-4 lg:mb-6'>
               <img src={CourseImg3} alt='' />
             </div>
