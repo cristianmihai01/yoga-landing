00167d65d77ccd391e9c10e3bb50e8a834877a3e
cristianmihai01 <dev.cristianmihai@gmail.com>
2022-08-15 11:12:58 +0300

    added

--- src/components/Newsletter.js
+++ src/components/Newsletter.js
@@ -8,9 +8,19 @@ const Newsletter = () => {
           <h4 className='text-[26px] text-white font-bold mb-[14px]'>
             Subscribe Our Newsletter
           </h4>
-          <p className='text-green-200'>
+          <p className='text-green-200 mb-12'>
             Subscribe our newsletter for further updates about us
           </p>
+          <form className='max-w-[752px] mx-auto relative flex flex-col lg:flex-row gap-y-6 p-4 gap-x-4'>
+            <input
+              type='text'
+              className='form-control lg:flex-1 border border-green-300 bg-transparent rounded pl-6 outline-none placeholder:text-green-200 text-white'
+              placeholder='Enter your email address'
+            />
+            <button className='btn btn-lg btn-orange text-white'>
+              Get started
+            </button>
+          </form>
         </div>
       </div>
     </section>
